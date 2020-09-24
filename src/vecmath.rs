3fd045ec1524cab873072836f7342f53c5496444
djugei <ddjugei@gmail.com>
2020-09-24 19:19:54 +0200

    added angle method for vectors

--- src/vecmath.rs
+++ src/vecmath.rs
@@ -5,7 +5,14 @@ pub type Vector = [f64; 2];
 /// calculates the length of a vector
 pub fn len(inp: Vector) -> f64 {
     validate(&inp);
-    ((inp[0] * inp[0]) + (inp[1] * inp[1])).sqrt()
+    len2(inp).sqrt()
+}
+
+/// calculates length*length of a vector
+/// which conveniently avoids a sqrt
+pub fn len2(inp: Vector) -> f64 {
+    validate(&inp);
+    (inp[0] * inp[0]) + (inp[1] * inp[1])
 }
 
 /// turns the input into a vector that has length 1
@@ -43,6 +50,15 @@ pub fn add(mut a: Vector, b: Vector) -> Vector {
     a
 }
 
+/// componet-wise sub
+pub fn sub(mut a: Vector, b: Vector) -> Vector {
+    validate(&a);
+    validate(&b);
+    a[0] -= b[0];
+    a[1] -= b[1];
+    a
+}
+
 /// scales a vector by a scalar
 pub fn scale(mut a: Vector, scalar: f64) -> Vector {
     validate(&a);
@@ -50,6 +66,47 @@ pub fn scale(mut a: Vector, scalar: f64) -> Vector {
     a[0] *= scalar;
     a[1] *= scalar;
     a
+}
+
+/// relative angle, in radians, range -pi - pi
+fn angle(base: Vector, other: Vector) -> f64 {
+    let delta = sub(other, base);
+
+    let len = len(delta);
+    let x = delta[0] / len;
+    let rad = x.asin();
+    let ypos = delta[1].is_sign_positive();
+
+    // fixme: this feels insanely ugly
+    if ypos {
+        rad
+    } else {
+        std::f64::consts::PI.copysign(x) - rad
+    }
+}
+
+#[test]
+fn angle_test() {
+    let steps = 100_000;
+    let inc = 1. / (steps as f64);
+    let pi = std::f64::consts::PI;
+    for i in -steps..steps {
+        let num = (i as f64) * inc;
+        let pinum = num * pi;
+        let (x, y) = pinum.sin_cos();
+
+        let a = angle([0., 0.], [x, y]);
+
+        let d = (pinum - a).abs();
+        assert!(
+            d < 0.00001,
+            "pinum: {}, a: {}, x: {}, d: {}",
+            pinum,
+            a,
+            x,
+            d
+        );
+    }
 }
 
 #[inline]
